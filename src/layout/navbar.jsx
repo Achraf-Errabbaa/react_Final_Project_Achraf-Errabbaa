0c9e8a8afcb9e153d0ba165df816ff31f3f809c9
mortyrex34 <achraferrabbaa1@gmail.com>
2024-09-14 12:33:52 +0100

    added usenavigate to the navbar

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { FaRegUserCircle } from "react-icons/fa";
 import { RiShoppingCart2Line } from "react-icons/ri";
+import { useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+    const navigate =useNavigate()
     return (
         <nav className='h-[100px]  w-[100%] z-30 bg-white  flex justify-center items-center gap-[400px] fixed'>
             <div className='font-bold text-4xl'>Fashe <span className='text-[#e65540]'>.</span></div>
             <div className=' gap-10 flex justify-center items-center cursor-pointer '>
-                <div className=' hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Home</a></div>
-                <div className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Shop</a></div>
-                <div className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Blog</a></div>
-                <div className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>About</a></div>
-                <div className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Contact</a></div>
+                <div onClick={()=>{navigate('/')}} className=' hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Home</a></div>
+                <div onClick={()=>{navigate('/shop')}} className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Shop</a></div>
+                <div onClick={()=>{navigate('/blog')}} className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Blog</a></div>
+                <div onClick={()=>{navigate('/about')}} className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>About</a></div>
+                <div onClick={()=>{navigate('/contact')}} className='hover:border-b-[1px] hover:border-black '><a href="" className='focus:text-red-500'>Contact</a></div>
 
             </div>
             <div className='flex justify-center items-center gap-10 text-2xl text-[#d0d0d0]'>
